c6cd8d3638fc06844d9677e4342bc3918951a09c
Sergei Gmyria <sergei.gmyria@finofo.com>
2026-02-10 17:31:19 -0700

    changed cdn, added gray mark

--- emails/_config/consts.ts
+++ emails/_config/consts.ts
@@ -9,12 +9,12 @@ export const ASSETS = {
 	"finofo-mark-black-text": {
 		local: "/static/finofo-mark-black-text.png",
 		cid: "finofo-mark-black-text",
-		cdn: 'https://s3.us-east-2.amazonaws.com/production-email-content-finofo-emails.com/email-images/logo/finofo-mark-black-text.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAQDJNDR66WK7A4PFZ%2F20260210%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20260210T172709Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEOH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMiJGMEQCIDZTVfofiIikoBqR7%2Fw6eOfYHOwAnJq6ctCUS6rfXSPcAiBb1r3219JU6%2FjoAMGiuwBi4SrdreCZ%2FBHhOcXfBXcwHCr6Agir%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAEaDDAwNzA3MzkyNzEwMSIMYUw7Rpf%2FMawAYp6AKs4CigdCsSGrzFNLzw%2F7ZnXQY3uAgNiFNOlsffBePJHBYVRI2HZcyz0%2FY2omi2TrLWmytPTi0nNDtnBetAvcsLfpvk5bWFX7jHcXYkyegtvoEErkcRelBCC8UdALxjZhwRIPUXB1OzcIospPe4%2FdoOYoYLfBgYjdfbWkuHYeI2ZP5o5ULmdYlSTRzj8OoG6v4Aq0g8Ys7Nzrc%2FPN5pVk2eK21TIlraTUl%2BBAvamvmY9WrH845pBhGwR%2Fu7qzLHdb9b04qOn9XVdp2mIiVVJrFGCpFjC8P3Hmk4A8NtGFIvRwR6fsVwnoztSk5fXI38B1vZ5l6WYHUS6ZyBsJsKP0oe8iDl%2F5qwz%2F1O88eGnW38ythLhj8byMydYg%2FZtLSGtcD3XocGzFCpo3%2F%2FSzruX4GhZueXKp8MAixNTXmWgL8RUKklOptSHyECpCOZXJftbo8zCls63MBjquAvY3cVWu4JqM%2BERcl8oNV9tI%2FJErreo2HiX7xcqU7zZ94Yen5fmB1I7tAD%2FmThGbprVvaxn9%2F%2F2iRoQ9llRljq4j8kiAAKJTArQiVr2YZ%2B9qVmjVVRRnoZ77rKYfAJL%2Fcf7GvOfbAD4AhjAd%2BJKNhSwDFRqM1jM11O5muNMJ6Z%2FNB34znTpDQoNhm660bTNM4iEzOWnA1EXfgbamP5s7MkjCQXzt1CuLXuyzmAfOGKmSBTWkDQOzX0q7kzUrLD6NIIRj%2BMh0msE71hauowylJ9CF35LgzLBX2a2bTQfp3hFYXXDJJRnWRWAxIfGOtMrqvh6PUWfUcpq12JYLuqAITS5deFkp3QkMLURn3fIDOvfmJYTb7iyICPw%2FVe8cc0%2Ff%2FJpXZabdo%2FckYMMB1tEm&X-Amz-Signature=a9a0c0aa85f624fd285b65fde211f3f8eb0a03b0f09b8e2f8c247d0de2f3f064&X-Amz-SignedHeaders=host&response-content-disposition=inline'
+		cdn: 'https://finofo-emails.com/email-images/logo/finofo-mark-black-text.png'
 	},
 	"finofo-mark-gray": {
 		local: "/static/finofo-mark-gray.png",
 		cid: "finofo-mark-gray",
-		cdn: ''
+		cdn: 'https://finofo-emails.com/email-images/logo/finofo-mark-gray.png'
 	},
 	"finofo-mark-tiny": {
 		local: "/static/finofo-mark-tiny.png",
@@ -24,7 +24,7 @@ export const ASSETS = {
 	"finofo-signature": {
 		local: "/static/finofo-signature.png",
 		cid: "finofo-signature",
-		cdn: 'https://s3.us-east-2.amazonaws.com/production-email-content-finofo-emails.com/email-images/misc/finofo-signature.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAQDJNDR66UZPX2RPS%2F20260210%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20260210T172737Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEOH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMiJIMEYCIQCJhKOE8uenafpAJwsh1s8WrkxgngX3xtZWReM8HDnB6wIhAMhmhYZk7hnEWLbUI5B3OwMZeLZOa%2FWrHGZ6Erw91b%2ByKvoCCKv%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQARoMMDA3MDczOTI3MTAxIgyidL0IZej5Gm4VjTMqzgIeYBkkjRi09ViX7sjhsNiN7ipm8KFKKhOEPf%2FOLNHYTtzePUr1Y3rzPbrpy0WYTMajcZ7e2oCQX7EgHS0TEnBvZZoz9U4iqsA%2FVpxjOCkDyKLFUNdTkP3GbgonPT9mgfg4A8Q8LLazR0d0R70BUR8UXIeaGfJETZy2cvdW%2Fe9%2Bq9X%2BsqBlId05xVCY4oHYUXNGvyB87QIkXBygxsG7Ht1s9jtCAa4lgCRabOF9z9HPrG8ad%2FBmHRhYy%2BE%2BN5qVXhNeLKvwodf8BRlGanDfx0FS0bvr9YboZ4ASqXwz5xBhkpVjjhBlKakxReAjlLOgJc5Y7RL%2FWSPknJA7M3OzScwDuJl%2BXPTNXB9RwMiY6IY9tJ9i%2Bco9ntswPDsVJ1ZmJIEdrYoUP%2FhVPv45K4ZyIss85qcmYHp5RwPUIETWDjIemT7jHfB4jMw172u%2FBfHcMKWzrcwGOqwCRs0lMsffFoNKT6u9n%2B4HSiAv2SuDhP90XiqySjy4PhstMnC9dQuK8lR%2F%2FaAW%2Fl7cqtKrR2503jGaylj073J4Kli4xcDPv4nimOSpAr85UWuZuqTgSdUeQCrHupz8PEyvSgurdhQAo9nqIKUWynv1ADxkUhfHCFt5m6F2Lwchcgqp9eNMf%2F%2FDA5iGCx8oNZfFEycP9vUUkbXj4Z06AZdxNgGFKX7JB3Q%2FNcjI6X9yUZfa8USEiV0jOTlF22wADjbDtkgfz%2BNRMyrkLqnFaeCCwhl6s9mCgYF6gybs%2BjssMvdRCSfkJU37VLhbmINlOYrfqvP0fMV2SeDIwzqCQmn68eH0Qsg1CXEbZJr9dKpdg%2FZ%2BR8eiBKbDvGb9%2BklD1McCvVo1UKBCxpkE4hak&X-Amz-Signature=e6a4668ac0c09f1f3a20c29589d546de59fee5e819dc9a4d07a1a3a9fe29d321&X-Amz-SignedHeaders=host&response-content-disposition=inline'
+		cdn: 'https://finofo-emails.com/email-images/misc/finofo-signature.png'
 	},
 	"finofo-payment-method": {
 		local: "/static/finofo-payment-method.png",
